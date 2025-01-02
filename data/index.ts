1ea1e6f181c24eb07a3418018595176ce7721cda
Sudhanshu Singh <sudhanshu1182abc@gmail.com>
2025-01-02 23:56:57 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,196 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Coding Profile", link: "#coding_profile" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Sudhanshu Singh",
+    description: "FullStack Developer",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+    btn: "Contact Me"
+  },
+
+  {
+    id: 5,
+    title: "Currently working on some real-world projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "LiveSync",
+    des: "Synchronize effortlessly with LiveSync, a real-time collaboration tool designed to connect and empower teams seamlessly.",
+    img: "/project1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/c.svg"],
+    link: "https://notion-clone-9vh0hkgm3-sudhanshu-singhs-projects-5b9bbfb1.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Learning Management System",
+    des: "Streamline your learning experience with our Learning Management Platform, featuring interactive courses and real-time progress tracking.",
+    img: "/project2.png",
+    iconLists: [
+      "js_img.png",
+      "/tail.svg",
+      "/mongoDB_img.png",
+      "/node-js.svg",
+      "/re.svg",
+    ],
+    link: "https://lms-d2xb.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Real-time Chat Application",
+    des: "Connect instantly with our real-time chat application, designed for seamless and efficient communication anytime, anywhere.",
+    img: "/project3.png",
+    iconLists: [
+      "js_img.png",
+      "/tail.svg",
+      "/mongoDB_img.png",
+      "/node-js.svg",
+      "/re.svg",
+    ],
+    link: "https://mern-chat-application-d046.onrender.com/",
+  },
+  {
+    id: 4,
+    title: "Frontend Code Editor",
+    des: "Code effortlessly with our frontend code editor, offering a smooth and interactive environment for writing, testing, and debugging your code.",
+    img: "/project4.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "js_img.png",
+      "/css_img.png",
+      "html_img.png",
+    ],
+    link: "https://code-editor-8cyb.onrender.com/",
+  },
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Leetcode ",
+    desc: "Tackled about 600 DSA problems and achieved the badge of Knight by continuously participating in contests",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+    link: "https://leetcode.com/u/sudhanshu1182/",
+  },
+  {
+    id: 2,
+    title: "Codeforces",
+    desc: "Solved about 300 coding problems and achieved the Specialist tag through consistent participation in contests",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+    link: "https://codeforces.com/profile/sudhanshu_singh1182",
+  },
+  {
+    id: 3,
+    title: "Codechef",
+    desc: "Solved about 100 coding problems and got the 3-star rating",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+    link: "https://www.codechef.com/users/sudhanshu_1182",
+  },
+  {
+    id: 4,
+    title: "GeeksForGeeks",
+    desc: "Tackled more than 600 Data structures and Algorithm problems ",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+    link: "https://www.geeksforgeeks.org/user/ss0077350/",
+  },
+
+  {
+    id: 5,
+    title: "Atcoder",
+    desc: "Regularly participated in contests and achieved 6-kyu rating ",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+    link: "https://atcoder.jp/users/sudhanshu_s1182",
+  },
+
+  {
+    id: 6,
+    title: "CodeStudio",
+    desc: "Solved about 200 coding problems ",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+    link: "https://www.naukri.com/code360/profile/d311f2b8-0f42-488d-86ae-ed8bd451b322",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Sudhanshusingh1182",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/SudhanshuS1182?t=JeNxrCoiD5iJ8ORKnjGeCg&s=09",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/sudhanshu-singh-3b2863228",
+  },
+];
